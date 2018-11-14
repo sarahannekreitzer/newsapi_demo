49d8540b5382f80c4bf3d418692768a483a1c273
Sarah Kreitzer <sarah.anne.kreitzer@gmail.com>
2018-11-14 12:38:26 -0500

    first hide setup

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function(){
+  console.log('scripts loaded');
+
+  var myKey = config.MY_KEY; // linking to hidden config file
+
+  var url =
+  'https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=5bc68bc44be6441dbae5d5ea1edeeab4' + myKey;
+
+
+});
